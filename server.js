b28e394d389834e7a258564f9f57d5a9619d6895
hechem05 <hechem150@gmail.com>
2026-06-03 23:54:09 +0100

    initial

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const BOT_TOKEN = "8925461538:AAEjsJ3-ro3ZHJE4Z9xGlik6V4cq1up2_D8";
+
+const CHAT_IDS = [
+    "7822574012"
+];
+
+app.post("/visit", async (req, res) => {
+
+    try {
+
+        const v = req.body;
+
+        const text = `
+🚨 NEW VISITOR
+
+🌐 Website:
+${v.website || "Unknown"}
+
+🌍 Country: ${v.country}
+🏙 City: ${v.city}
+
+📱 Device: ${v.device}
+
+🔗 Page:
+${v.page}
+
+🕒 ${new Date().toLocaleString()}
+`;
+
+        for (const id of CHAT_IDS) {
+
+            await axios.post(
+                `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`,
+                {
+                    chat_id: id,
+                    text: text
+                }
+            );
+        }
+
+        res.status(200).json({ success: true });
+
+    } catch (err) {
+
+        console.log(err.message);
+
+        res.status(500).json({ success: false });
+    }
+});
+
+app.get("/", (req, res) => {
+    res.send("Visitor Tracker Running");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Running on port ${PORT}`);
+});
